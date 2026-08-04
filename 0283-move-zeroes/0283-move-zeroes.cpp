c033e0fc8f8a3024da82da5eef9606a9ec89ea8d
Dhruv Kapure <dhruvkapure3@gmail.com>
2026-08-04 11:53:52 +0530

    Time: 0 ms (100%), Space: 23.7 MB (98.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> temp;
-
-        for(int x: nums)
+        int j = -1;
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(x != 0)
-            temp.push_back(x);
+            if (nums[i] == 0){
+                j = i;
+                break;
+            }
         }
-
-        while(temp.size() < nums.size())
+        if(j == -1) return;
+        for(int i = j+1; i < nums.size(); i++)
         {
-            temp.push_back(0);
+            if(nums[i] != 0)
+            {
+                swap(nums[i], nums[j]);
+                j++;
+            }
         }
-        nums = temp;
     }
 };
