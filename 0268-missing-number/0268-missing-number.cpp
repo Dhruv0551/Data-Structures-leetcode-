1a02f62dab4445df9bf4033093097827cb244c6d
Dhruv Kapure <dhruvkapure3@gmail.com>
2026-08-07 18:49:33 +0530

    Time: 0 ms (100%), Space: 21.9 MB (39.48%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,9 +4,9 @@ public:
         short int sum1 = 0;
         short int sum2 = 0;
 
+        sum1 = (nums.size())  * ((nums.size()) + 1) / 2;
         for (int i = 1; i <= nums.size(); i++)
         {
-            sum1+=i;
             sum2+=nums[i - 1];
         }
 
