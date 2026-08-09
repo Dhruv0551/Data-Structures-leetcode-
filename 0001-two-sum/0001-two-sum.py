db08525ee4fe64584664fcd912d46004acffebd7
Dhruv Kapure <dhruvkapure3@gmail.com>
2026-08-09 23:36:20 +0530

    Time: 3 ms (53.77%), Space: 20.4 MB (58.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map_dict = {}
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            curr = nums[i]
+            more = target - curr
+
+            if more in map_dict:
+                return [i, map_dict[more]]
+
+            map_dict[curr] = i
 
         return -1
