5db5017a949913b35e9b8a306111e6181e98533d
Dhruv Kapure <dhruvkapure3@gmail.com>
2026-08-10 14:35:02 +0530

    Time: 17 ms (8.48%), Space: 21.3 MB (18.57%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums_dict = {}
+
+        for i in nums:
+            if i not in nums_dict:
+                nums_dict[i] = 1
+            else:
+                nums_dict[i]+=1
+
+        for key, value in nums_dict.items():
+            if value > len(nums) / 2:
+                return key
