3ebbd834b47f980bc32cefc4079bc5f2caca08e6
Dhruv Kapure <dhruvkapure3@gmail.com>
2026-08-16 00:10:37 +0530

    Time: 0 ms (100%), Space: 21 MB (23.64%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,61 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int> row_vec(matrix.size(), 0);
-        vector<int> col_vec(matrix[0].size(), 0);
-
+        bool rowZero = false, colZero = false;
         for(int i = 0; i < matrix.size(); i++)
         {
-            for(int j = 0; j < matrix[0].size(); j++)
+            if(matrix[i][0] == 0){
+                rowZero = true;
+                break;
+            }
+        }
+        for(int i = 0; i < matrix[0].size(); i++)
+        {
+            if(matrix[0][i] == 0){
+                colZero = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i < matrix.size(); i++)
+        {
+            for(int j = 1; j < matrix[0].size(); j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    row_vec[i] = 1;
-                    col_vec[j] = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-
-        for(int i = 0; i < matrix.size(); i++)
+        for(int i = 1; i < matrix.size(); i++)
         {
-            for(int j = 0; j < matrix[0].size(); j++)
+            for(int j = 1; j < matrix[0].size(); j++)
             {
-                if(row_vec[i] or col_vec[j])
+                if(matrix[i][0] == 0 or matrix[0][j] == 0)
+                {
                     matrix[i][j] = 0;
+                }
             }
         }
+         if(colZero)
+        {
+            for(int i = 0; i < matrix[0].size(); i++)
+            {
+                matrix[0][i] = 0;
+            }
+        }
+        
+        if(rowZero)
+        {
+            for(int i = 0; i < matrix.size(); i++)
+            {
+            matrix[i][0] = 0;
+            }
+        }
+
+       
+
     }
 };
