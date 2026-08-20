f4c6d1d62e344b3001e15d9170def6f5630bbdfd
Dhruv Kapure <dhruvkapure3@gmail.com>
2026-08-20 22:54:04 +0530

    Time: 9 ms (23.37%), Space: 26.6 MB (10.92%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,15 +4,11 @@ public:
         int maxCriteria = nums.size() / 3;
         vector<int> ans;
         unordered_map<int, int> mpp;
-        for(int &x: nums)
+        for(int &x: nums){
             mpp[x]++;
-
-        for(const auto& [key, value]: mpp)
-        {
-            if(value > maxCriteria)
-                ans.push_back(key); 
+            if(mpp[x] == maxCriteria + 1)
+                ans.push_back(x);
         }
-
         return ans;
     }
 };
