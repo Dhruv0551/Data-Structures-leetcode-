27e25650b8c9847e4d45d182d19a233102af9c67
Dhruv Kapure <dhruvkapure3@gmail.com>
2026-08-20 23:12:42 +0530

    Time: 0 ms (100%), Space: 23.7 MB (58.77%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,14 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int maxCriteria = nums.size() / 3;
+        int count1 = 0, count2 = 0, el1 = INT_MIN, el2 = INT_MIN;
+        int minRequirement = (nums.size() / 3);
         vector<int> ans;
-        unordered_map<int, int> mpp;
-        for(int &x: nums){
-            mpp[x]++;
-            if(mpp[x] == maxCriteria + 1)
-                ans.push_back(x);
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(count1 == 0 && nums[i] != el2)
+            {
+                count1 = 1;
+                el1 = nums[i];
+            }
+
+            else if(count2 == 0 && nums[i] != el1)
+            {
+                count2 = 1;
+                el2 = nums[i];
+            }
+
+            else if(nums[i] == el1) count1++; 
+            else if(nums[i] == el2) count2++;
+            else {
+                count1--, count2--;
+            }
         }
+
+        count1 = 0, count2 = 0;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == el1) count1++;
+            if(nums[i] == el2) count2++;
+        }        
+
+        if(count1 > minRequirement) ans.push_back(el1);
+        if(count2 > minRequirement) ans.push_back(el2);
+
         return ans;
     }
 };
