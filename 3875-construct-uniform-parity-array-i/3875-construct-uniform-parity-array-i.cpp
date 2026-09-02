36cbc595445dad68bec4a83c18a413e5333510b4
Dhruv Kapure <dhruvkapure3@gmail.com>
2026-09-02 21:12:00 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // bool isOdd = false;
+
+        // for(int x: nums1)
+        // {
+        //     if(x % 2 != 0)
+        //     {
+        //         isOdd = true;
+        //         break;
+        //     }
+        // }
+
+        // if (!isOdd) return true;
+        return true;
+    }
+};
